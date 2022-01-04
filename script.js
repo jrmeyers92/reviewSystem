df189e375cc04eb65d50e79a00543ae3d03efd70
Jake Meyers <JakeRMeyers92@gmail.com>
2022-01-04 12:44:47 -0600

    slide and form validation functional

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// set review response message and show form to fill out.
 if (document.getElementById('stars')) {
+  // set review response message and show form to fill out.
   let messages = {
     1: "Oh no! Please tell us how we can improve.",
     2: "Unsatisfactory. Please tell was how we can improve.",
@@ -13,23 +13,47 @@ if (document.getElementById('stars')) {
       let response = document.getElementById('reviewResponse');
       document.getElementById('slider').classList.remove('hidden');
 
-      if (e.target.id == "rating-star-1") {
-        response.innerHTML = messages[1];
-      } else if (e.target.id == "rating-star-2") {
-        response.innerHTML = messages[2];
-      } else if (e.target.id == "rating-star-3") {
-        response.innerHTML = messages[3];
-      } else if (e.target.id == "rating-star-4") {
-        response.innerHTML = messages[4];
-      } else if (e.target.id == "rating-star-5") {
-        response.innerHTML = messages[5];
+      switch (e.target.id) {
+        case "rating-star-1": 
+          response.innerHTML = messages[1];
+          break; 
+        case "rating-star-2": 
+          response.innerHTML = messages[2];
+          break; 
+        case "rating-star-3": 
+          response.innerHTML = messages[3];
+          break; 
+        case "rating-star-4": 
+          response.innerHTML = messages[4];
+          break; 
+        case "rating-star-5": 
+          response.innerHTML = messages[5];
+          break; 
       }
   })
 } )
+// FORM VALIDATIO -- enables next button when form is filled out properly
+let events = 'keyup select change'.split(' ');
+events.forEach(event => {
+  document.getElementById('reviewQuestions').addEventListener(event, () => {
+    let inputs = document.querySelectorAll('#reviewQuestions input, #reviewQuestions textarea, #reviewQuestions select');
+    let validity = true;
+  
+    for (let i = 0; i < inputs.length; i++) {
+      if (!inputs[i].checkValidity()) {
+        validity = false;
+        document.getElementById('nextButton').disabled = true;
+        break;
+      }
+    }
+  
+    if (validity === true) {
+      document.getElementById('nextButton').disabled = false;
+    }
+  })
+}) 
 
-
-
-
+// slider form functionality 
   const nextButton = document.getElementById('nextButton');
   const prevButton = document.getElementById('prevButton');
   const slideWrapper = document.getElementById('slideWrapper');
